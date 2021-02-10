3259b3e32a2afdeec8cd61061d8d074742ae1195
ahmaddynugroho <ahmaddwi700@gmail.com>
2021-02-10 12:47:25 +0700

    feat: add Button.vue component

---
+++ src/components/UI/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button :class="classes">
+    <i :class="faIconTop"></i>
+    <p>
+      {{ title }}
+    </p>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    faIconLeft: String,
+    faIconTop: String,
+    classes: String,
+  },
+}
+</script>
