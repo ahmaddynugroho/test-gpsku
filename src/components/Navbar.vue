a0306688abe985a556fcb95bcd90eda1fbd7dce6
ahmaddynugroho <ahmaddwi700@gmail.com>
2021-02-11 14:11:58 +0700

    feat: implement dropdown component in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,77 +1,122 @@
 <template>
-  <div class="container mx-auto flex justify-between py-4 relative">
-    <div>
-      <ul class="font-semibold text-gray-800 text-sm">
-        <li class="inline-block mr-8 cursor-pointer">
-          Master <i class="ml-1 fas fa-chevron-down fa-xs"></i>
-        </li>
-        <li class="inline-block mr-8 cursor-pointer">
-          Report <i class="ml-1 fas fa-chevron-down fa-xs"></i>
-        </li>
-        <li class="inline-block mr-8 cursor-pointer">
-          Billing <i class="ml-1 fas fa-chevron-down fa-xs"></i>
-        </li>
-        <li class="inline-block mr-8 cursor-pointer">
-          HRD <i class="ml-1 fas fa-chevron-down fa-xs"></i>
-        </li>
-      </ul>
+  <div class="nav-container">
+    <!-- LOGO -->
+    <div class="nav-logo">GPSKU</div>
+
+    <!-- LEFT MENUS -->
+    <div class="flex">
+      <Dropdown
+        v-for="(menu, i) in dropdownMenus"
+        :key="i"
+        class="mr-2 px-2 py-1 rounded hover:bg-gray-200"
+        :title="menu.title"
+      >
+        <ul class="bg-white rounded shadow">
+          <li v-for="(sub, j) in menu.subMenu" :key="j">
+            <Button class="nav-button" :title="sub.title" />
+          </li>
+        </ul>
+      </Dropdown>
     </div>
-    <div>
-      <ul class="font-semibold text-gray-800 text-sm">
-        <li class="inline-block mr-8 cursor-pointer">
-          <i class="fas fa-pencil-ruler"></i>
-          <i class="ml-1 fas fa-chevron-down fa-xs"></i>
-        </li>
-        <li class="inline-block mr-8 cursor-pointer">
-          <i class="fas fa-language fa-lg"></i>
-          <i class="ml-1 fas fa-chevron-down fa-xs"></i>
-        </li>
-        <li
-          class="relative pl-5 inline-block mr-8 cursor-pointer"
-          id="user"
-          @click="dropMenu"
-        >
-          <div
-            class="absolute h-6 w-6 rounded-full bg-gray-500 inline-block transform -translate-x-8"
-          ></div>
-          Administrator <i class="ml-1 fas fa-chevron-down fa-xs"></i>
-          <ul
-            class="hidden absolute rounded min-w-full right-0 transform translate-y-2"
-          >
-            <li class="px-4 py-2 text-gray-800 rounded hover:bg-gray-200">
-              Menu numb 1
-            </li>
-            <li class="px-4 py-2 text-gray-800 rounded hover:bg-gray-200">
-              Menu numb 2
-            </li>
-            <li class="px-4 py-2 text-gray-800 rounded hover:bg-gray-200">
-              Menu numb 3
-            </li>
-          </ul>
-        </li>
-      </ul>
+
+    <!-- RIGHT MENUS -->
+    <div class="flex">
+      <Dropdown
+        v-for="(menu, i) in dropdownMenus2"
+        :key="i"
+        class="mr-2 px-2 py-1 rounded hover:bg-gray-200"
+        :faIcon="menu.faIcon"
+        :title="menu.title"
+      >
+        <template v-if="i === 2" v-slot:prefix>
+          <i class="fas fa-circle fa-2x"></i>
+        </template>
+        <ul class="bg-white rounded shadow">
+          <li v-for="(sub, j) in menu.subMenu" :key="j">
+            <Button class="nav-button" :title="sub.title" />
+          </li>
+        </ul>
+      </Dropdown>
     </div>
-    <div class="absolute min-w-full text-center" id="logo">GSPKU</div>
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    dropMenu() {
-      const e = document.querySelector('li#user i')
-      e.classList.toggle('transform')
-      e.classList.toggle('rotate-180')
+import Dropdown from '../components/UI/Dropdown'
+import Button from '../components/UI/Button'
 
-      const f = document.querySelector('li#user ul')
-      f.classList.toggle('hidden')
-    },
+export default {
+  components: {
+    Dropdown,
+    Button,
+  },
+  data() {
+    return {
+      dropdownMenus: [
+        {
+          title: 'Master',
+          subMenu: [
+            { title: 'Sub Menu 1' },
+            { title: 'Sub Menu 2' },
+            { title: 'Sub Menu 3' },
+            { title: 'Sub Menu 4' },
+          ],
+        },
+        {
+          title: 'Report',
+          subMenu: [
+            { title: 'Sub Menu 1' },
+            { title: 'Sub Menu 2' },
+            { title: 'Sub Menu 3' },
+            { title: 'Sub Menu 4' },
+          ],
+        },
+        {
+          title: 'Billing',
+          subMenu: [
+            { title: 'Sub Menu 1' },
+            { title: 'Sub Menu 2' },
+            { title: 'Sub Menu 3' },
+            { title: 'Sub Menu 4' },
+          ],
+        },
+        {
+          title: 'HRD',
+          subMenu: [
+            { title: 'Sub Menu 1' },
+            { title: 'Sub Menu 2' },
+            { title: 'Sub Menu 3' },
+            { title: 'Sub Menu 4' },
+          ],
+        },
+      ],
+      dropdownMenus2: [
+        {
+          faIcon: 'fas fa-pencil-ruler',
+          subMenu: [{ title: 'sub 1' }, { title: 'sub 2' }],
+        },
+        {
+          faIcon: 'fas fa-language',
+          subMenu: [{ title: 'sub 1' }, { title: 'sub 2' }],
+        },
+        {
+          title: 'Administrator',
+          subMenu: [{ title: 'sub 1' }, { title: 'sub 2' }],
+        },
+      ],
+    }
   },
 }
 </script>
 
 <style scoped>
-#logo {
-  z-index: -1;
+.nav-container {
+  @apply container mx-auto flex justify-between relative;
+}
+.nav-logo {
+  @apply absolute py-1 w-full text-center;
+}
+.nav-button {
+  @apply w-full px-4 py-2 hover:bg-gray-200 text-left;
 }
 </style>
